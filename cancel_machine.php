6f3928d9dbc66e675637c9cae7d175370b88d9d6
whang23 <whchuah06@gmail.com>
2025-06-14 15:36:55 +0800

    cancel machine redirect for control+controlpanel

--- cancel_machine.php
+++ cancel_machine.php
@@ -3,8 +3,16 @@ include 'functions.php';
 
 $machine_id = intval($_POST['id']);
 $hb = intval($_POST['hb']);
+$redirect_page = isset($_POST['redirect']) ? $_POST['redirect'] : 'control_panel';
 
 cancelMachine($machine_id);
-header("Location: control_panel.php?hb=$hb");
+
+// Determine redirect destination
+$redirect_url = match($redirect_page) {
+    'control' => "control.php?id=$machine_id&hb=$hb",
+    default => "control_panel.php?hb=$hb"
+};
+
+header("Location: $redirect_url");
 exit;
-?> 
+?>
